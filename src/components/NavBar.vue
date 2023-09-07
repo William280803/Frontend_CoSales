d8cb681ff40e83a965589bc5c4102f7d4862781b
William <williamalim410@gmail.com>
2023-09-07 22:11:45 +0700

    START/Trello-CoSales_FE-0

---
+++ src/components/NavBar.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-app-bar app>
+        <v-toolbar-title>CoSales</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn text to="/home">Home</v-btn>
+        <v-btn text to="/shop">Shop</v-btn>
+        <v-btn text to="">Contact</v-btn>
+    </v-app-bar>
+</template>
+  
+<script>
+
+</script>
+  
---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+// Composables
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/home',
+    component: () => import('@/layouts/default/Default.vue'),
+    children: [
+      {
+        path: '',
+        name: 'Home',
+        component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
+      },
+    ],
+  },
+  {
+    path: '/Shop',
+    component: () => import('@/layouts/default/Default.vue'),
+    children: [
+      {
+        path: '',
+        name: 'Shop',
+        component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
+      },
+    ],
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+})
+
+export default router
